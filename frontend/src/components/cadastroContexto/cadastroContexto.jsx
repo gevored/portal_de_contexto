f0649227871000022de47e06185e944e2dbf83cd
gevored <gersonrevoredo@hotmail.com>
2020-05-25 17:42:55 -0300

    cadastro
    inclusão dos demais campos de cadastro de contexto

--- frontend/src/components/cadastroContexto/cadastroContexto.jsx
+++ frontend/src/components/cadastroContexto/cadastroContexto.jsx
@@ -7,13 +7,24 @@ class CadastroContexto extends React.Component {
     constructor(props) {
       super(props);
       this.state = {nomeCampanha: ''};
-      this.state = {segmento: 'n/a'};
+      this.state = {segmento: 'DEFAULT'};
       this.state = {contexto: ''};
+
+      this.state = {inicioVigencia: ''};
+      this.state = {fimVigencia: ''};
+      this.state = {produto: 'DEFAULT'};
+
+
 
 
       this.selectSegmento = this.selectSegmento.bind(this);
       this.setNomeCampanha = this.setNomeCampanha.bind(this);
       this.setContexto = this.setContexto.bind(this);
+
+
+      this.setInicioVigencia = this.setInicioVigencia.bind(this);
+      this.setFimVigencia = this.setFimVigencia.bind(this);
+
 
       this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -28,46 +39,92 @@ class CadastroContexto extends React.Component {
 
     setContexto(event) {
         this.setState({contexto: event.target.value});
-      }
+    }
+      
+    setInicioVigencia(event) {
+        this.setState({inicioVigencia: event.target.value});
+    }
+
+    setFimVigencia(event) {
+        this.setState({fimVigencia: event.target.value});
+    }
 
     handleSubmit(event){
         alert('Campanha Cadastrada');
         event.preventDefault();
     }
-  
+
     render() {
       return (
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit}  >
         <div>
-            <div class="form-row">
-                <div class="col">
+            <div >
+                <h2> Cadastro de Contextos </h2>
+                <div className = "flexVertical">
+                <div  className= "flexVertical">
                     <label>
                     Nome da campanha:
-                    <input class="form-control" type="text" value={this.state.nomeCampanha} onChange={this.setNomeCampanha} />
                     </label>
+                    <input  type="text" value={this.state.nomeCampanha} onChange={this.setNomeCampanha} />
                 </div>
-                <div class="col">
-                    <label>
+                    <div  className= "flexVertical">
+                    <label >
                         Segmento: 
-                        <select class="form-control" value={this.state.segmento} onChange={this.selectSegmento}>
+                    </label>
+                    
+                        <select value={this.state.segmento} onChange={this.selectSegmento}>
+                            <option value="DEFAULT">Selecione um Segmento</option>
                             <option value="IA">Itaú Agências</option>
                             <option value="IU">Uniclass</option>
-                            <option value="n/a">Selecione um Segmento</option>
+                           
                         </select>
-                    </label>
+                    </div>
                 </div>
             </div>
-            <div class="form-row" >
+            <div  className= "flexVertical">
                 <label>
                 Contexto:
                 </label>
-                <textarea class="form-control"  value={this.state.contexto} onChange={this.setContexto} />
+                <textarea  value={this.state.contexto} onChange={this.setContexto} />
+               
             </div>
-            <div>
+            <div className= "flexVertical">
+                <div  className= "flexVertical">
+                <label>
+                    Início de vigência:
+                </label>
+                <input  type="date" value={this.state.inicioVigencia} onChange={this.setInicioVigencia} />
+                </div>
 
+                <div  className= "flexVertical">
+                <label>
+                    Fim de vigência
+                </label>
+                
+                <input  type="date" value={this.state.fimVigencia} onChange={this.setFimVigencia} />
+                </div>
             </div>
-        
- 
+
+            <div  className= "flexVertical"> 
+                <label>
+                    Produto: 
+                </label>
+                
+                    <select value={this.state.produto} onChange={this.selectSegmento}>
+                        <option value="RENEG">Renegociação</option>
+                        <option value="SEGUROS">Seguros</option>
+                        <option value="CREDIARIO">Crediário</option>
+                        <option value="CONSIG_INSS">Consignado INSS</option>
+                        <option value="CONSIG_EP">Consignado EP</option>
+                        <option value="CONSIGNADO_OP">Consignado OP</option>
+                        <option value="CONSIGNADO_EP_OP">Consignado OP/EP</option>
+                        <option value="DEFAULT">Selecione um produto</option>
+                    </select>
+            </div>
+
+            <div className = "classButon">            
+                <input  type="submit" value="Cadastrar" />
+            </div>
         </div>
       </form>
       );
